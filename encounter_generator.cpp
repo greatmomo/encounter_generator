28d3382f4493cbc45c271d0ac49fff8114ae7fc1
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-09-27 20:18:42 +0200

    stable upload
    after fix of gui tries

--- encounter_generator.cpp
+++ encounter_generator.cpp
@@ -7,3 +7,11 @@ int main()
 {
     Encounter generatedEncounter = Encounter(Encounter::Region::Plains); // test with plains before implementing others
 }
+
+// find out how to make windows (I have a bookmark)
+// make syntax for standardized monster/trap/etc reading (ex. everything between two '#' symbols)
+// figure out how to make monster/trap names bold
+// implement reading out of which monsters or traps should be posted
+// create monster and trap classes that post stat blocks
+// 
+// allow input of custom entries that are then saved to the proper files?
