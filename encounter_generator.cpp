c3f82286f8d072b4afce0f7fb71d356c2e24e783
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-10-15 20:59:45 +0200

    Some comment changes

--- encounter_generator.cpp
+++ encounter_generator.cpp
@@ -11,7 +11,7 @@ int main()
     AvailableDirectories aD("\RegionData");
     std::vector<std::string> availableRegions = aD.GetAvailableDirectories();
 
-    Encounter generatedEncounter = Encounter(availableRegions); // test with plains before implementing others
+    Encounter generatedEncounter = Encounter(availableRegions);
 
     AvailableStatblocks aS("\Monsters");
     std::vector<std::string> availableStatblocks = aS.GetAvailableStatblocks();
@@ -27,11 +27,10 @@ int main()
 }
 
 // Still need to implement the Random option
+// Add some sort of loop to the whole program?
 // 
 // find out how to make windows (I have a bookmark) - it's impossible for some reason
 // add traps?
 // add obstacles or other things with DCs?
-// 
-// allow input of custom entries that are then saved to the proper files?
 //
 // Search TODO
